5fa8975c0efc5901cd7921e5318d5fe396c8cac6
soo970220 <soo970220@gmail.com>
2026-05-20 17:09:18 +0900

    260520 [develop] meerkatgram 삭제건 저장

---
+++ src/main/resources/schema/v000/260519-01-create-uesrs.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `users`
+(
+     id            BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT
+    ,email         VARCHAR(100) NOT NULL UNIQUE KEY
+    ,password      VARCHAR(255) NOT NULL
+    ,nick          VARCHAR(20)  NOT NULL
+    ,provider      VARCHAR(10)  NOT NULL DEFAULT 'NONE'
+    ,role          VARCHAR(10)  NOT NULL DEFAULT 'NORMAL'
+    ,profile       VARCHAR(100) NOT NULL
+    ,refresh_token VARCHAR(255) NULL	DEFAULT NULL
+    ,created_at    DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP()
+    ,updated_at    DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP()
+    ,deleted_at    DATETIME	NULL DEFAULT NULL
+);
