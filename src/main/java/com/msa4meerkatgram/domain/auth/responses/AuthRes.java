2c1bf07e7be183b843faa262399f24fb48899636
soo970220 <soo970220@gmail.com>
2026-07-07 17:29:58 +0900

    JPA전환 작업중 (Repository 적용 및 ResponseDTO 관리 로직 변환)

--- src/main/java/com/msa4meerkatgram/domain/auth/responses/AuthRes.java
+++ src/main/java/com/msa4meerkatgram/domain/auth/responses/AuthRes.java
@@ -1,11 +1,18 @@
 package com.msa4meerkatgram.domain.auth.responses;
 
+import com.msa4meerkatgram.domain.user.entities.User;
 import com.msa4meerkatgram.domain.user.responses.UserRes;
+import com.msa4meerkatgram.domain.user.responses.UserWithPostCountRes;
 import lombok.Builder;
 
 @Builder
 public record AuthRes(
-    UserRes user
+    UserWithPostCountRes user
     ,String accessToken
-) {
+) { public static AuthRes from(User user, String accessToken, long countPosts) {
+
+    return new AuthRes(
+            UserWithPostCountRes.from(user, countPosts)
+            ,accessToken);
+}
 }
--- src/main/java/com/msa4meerkatgram/domain/post/repositories/PostRepository.java
+++ src/main/java/com/msa4meerkatgram/domain/post/repositories/PostRepository.java
@@ -1,8 +1,9 @@
 package com.msa4meerkatgram.domain.post.repositories;
 
 import com.msa4meerkatgram.domain.post.entities.Post;
+import com.msa4meerkatgram.domain.user.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    long countByUser (User user);
 }
